d692f3e473330f21c4dfe021aa8889c2c6655dfb
Mateusz Nawrocki <01187259@pw.edu.pl>
2024-11-30 13:31:33 +0100

    Start ClientApp code.

---
+++ ClientApp/app/src/main/java/app/Main.java
@@ -0,0 +1,18 @@
+package app;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        JFrame frame = new JFrame("Chess");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
+        frame.setVisible(true);
+
+        
+
+
+    }
+}
