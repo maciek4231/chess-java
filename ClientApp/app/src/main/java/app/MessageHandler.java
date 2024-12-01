7e03536be7638e6ac52222f5749fab3f23626e75
Mateusz Nawrocki <01187259@pw.edu.pl>
2024-12-02 00:20:55 +0100

    Implement starting the game.

--- ClientApp/app/src/main/java/app/MessageHandler.java
+++ ClientApp/app/src/main/java/app/MessageHandler.java
@@ -8,6 +8,7 @@ public class MessageHandler {
     ChessWebSocketClient client;
     Board board;
     ConnectWindow connectWindow;
+    int gameCode;
 
     public MessageHandler(ChessWebSocketClient client, Board board) {
         this.client = client;
@@ -24,6 +25,9 @@ public class MessageHandler {
                     break;
                 case "joinGameRes":
                     handleJoinGame(msg);
+                    break;
+                case "placementRes":
+                    handlePlacement(msg);
                     break;
                 default:
                     System.out.println("Unknown message type: " + type);
@@ -68,6 +72,7 @@ public class MessageHandler {
         }
         if (status == 0) {
             connectWindow.gameFound();
+            gameCode = code;
             board.setGameCode(code);
         } else {
             if (status == -1) {
@@ -76,5 +81,63 @@ public class MessageHandler {
                 connectWindow.opponentLeft();
             }
         }
+    }
+
+    private void handlePlacement(JsonObject msg) {
+        try {
+            for (int i = 0; i < 8; i++) {
+                String row = msg.get(Integer.toString(i)).getAsString();
+                for (int j = 0; j < 8; j++) {
+                    placePiece(row.charAt(j), new Coords(j, i));
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid placement received.");
+        }
+    }
+
+    private void placePiece(char type, Coords coords) {
+        PieceType pieceType;
+        switch (type) {
+            case 'P':
+                pieceType = PieceType.B_PAWN;
+                break;
+            case 'R':
+                pieceType = PieceType.B_ROOK;
+                break;
+            case 'N':
+                pieceType = PieceType.B_KNIGHT;
+                break;
+            case 'B':
+                pieceType = PieceType.B_BISHOP;
+                break;
+            case 'Q':
+                pieceType = PieceType.B_QUEEN;
+                break;
+            case 'K':
+                pieceType = PieceType.B_KING;
+                break;
+            case 'p':
+                pieceType = PieceType.W_PAWN;
+                break;
+            case 'r':
+                pieceType = PieceType.W_ROOK;
+                break;
+            case 'n':
+                pieceType = PieceType.W_KNIGHT;
+                break;
+            case 'b':
+                pieceType = PieceType.W_BISHOP;
+                break;
+            case 'q':
+                pieceType = PieceType.W_QUEEN;
+                break;
+            case 'k':
+                pieceType = PieceType.W_KING;
+                break;
+            default:
+                return;
+        }
+        board.addPiece(pieceType, coords);
     }
 }
