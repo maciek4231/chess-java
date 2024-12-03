73c6c7c3792cd666acef652b06fa02b872613674
Mateusz Nawrocki <01187259@pw.edu.pl>
2024-12-03 11:22:13 +0100

    Implement reading the server address from config

--- ClientApp/app/src/main/java/app/Main.java
+++ ClientApp/app/src/main/java/app/Main.java
@@ -1,16 +1,23 @@
 package app;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 import javax.swing.JFrame;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 public class Main {
 
     public static void main(String[] args) {
         ChessWebSocketClient client;
+        String server = getServerAddress();
+
         try {
-            client = new ChessWebSocketClient(new URI("ws://localhost:8887"));
+            client = new ChessWebSocketClient(new URI(server));
             setUpConnection(client);
             Game game = new Game(client);
 
@@ -34,5 +41,37 @@ public class Main {
                 e.printStackTrace();
             }
         }
+    }
+
+    private static String getServerAddress() {
+        FileInputStream file = null;
+        String server = "ws://localhost:8887";
+
+        try {
+            file = new FileInputStream("config.json");
+            byte[] data = new byte[file.available()];
+            file.read(data);
+            String string = new String(data);
+            JsonObject json = JsonParser.parseString(string).getAsJsonObject();
+            server = json.get("server").getAsString();
+            file.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("config.txt not found, using default server address");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (file != null)
+            {
+                try {
+                    file.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return server;
     }
 }
