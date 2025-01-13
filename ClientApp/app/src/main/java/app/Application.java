5554d3ace5f092ac85294b65100637c0b085702e
Mateusz Nawrocki <01187259@pw.edu.pl>
2025-01-13 22:18:33 +0100

    Implement showing awaitingConnection.

--- ClientApp/app/src/main/java/app/Application.java
+++ ClientApp/app/src/main/java/app/Application.java
@@ -8,6 +8,7 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Application {
@@ -15,6 +16,7 @@ public class Application {
     Game game;
     ChessWebSocketClient client;
     JFrame mainFrame;
+    JPanel awaitingConnectionPanel;
     JPanel mainPanel;
     CardLayout mainPanelLayout;
     JPanel menu;
@@ -24,16 +26,29 @@ public class Application {
     {
         this.client = client;
 
-        createComps();
+        mainFrame = new JFrame("illChess");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(1536, 1024);
+        mainFrame.setLayout(new BorderLayout());
 
         setUpConnection();
+        showAwaitingConnection();
+        loadImages();
+
+        createComps();
+        waitForConnection();
+
+        showGame();
     }
 
     private void setUpConnection() {
         client.connect();
+    }
+
+    private void waitForConnection() {
         while (!client.isOpen()) {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -42,10 +57,6 @@ public class Application {
 
     private void createComps() {
         game = new Game(client);
-
-        mainFrame = new JFrame("illChess");
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setSize(1536, 1024);
 
         mainPanel = new JPanel();
         mainPanel.setPreferredSize(new Dimension(1152, 1024));
@@ -61,10 +72,6 @@ public class Application {
         loginPanel.setBackground(Color.RED);
         mainPanel.add(loginPanel, "login");
 
-        mainFrame.setLayout(new BorderLayout());
-        mainFrame.add(menu, BorderLayout.WEST);
-        mainFrame.add(mainPanel, BorderLayout.CENTER);
-
         mainPanel.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
@@ -75,6 +82,31 @@ public class Application {
         });
 
         mainFrame.setVisible(true);
-        mainPanelLayout.next(mainPanel);
+        mainPanelLayout.show(mainPanel, "login");
+    }
+
+    private void showAwaitingConnection()
+    {
+        awaitingConnectionPanel = new JPanel();
+        awaitingConnectionPanel.setPreferredSize(new Dimension(1536, 1024));
+        awaitingConnectionPanel.setLayout(new BorderLayout());
+        JLabel label = new JLabel("Awaiting connection...");
+        label.setFont(label.getFont().deriveFont(64.0f));
+        awaitingConnectionPanel.add(label, BorderLayout.CENTER);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        mainFrame.add(awaitingConnectionPanel, BorderLayout.CENTER);
+
+        mainFrame.setVisible(true);
+    }
+
+    private void showGame() {
+        mainFrame.add(menu, BorderLayout.WEST);
+        mainFrame.add(mainPanel, BorderLayout.CENTER);
+        mainFrame.remove(awaitingConnectionPanel);
+    }
+
+    private void loadImages() {
+        IconLoader.getMoveIcon();
     }
 }
