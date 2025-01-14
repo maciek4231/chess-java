8c7be967430440bbda7782f384e610558811c1b7
mdyduch1 <01187219@pw.edu.pl>
2025-01-14 05:29:00 +0100

    minor formatting

--- chess_server/src/main/java/com/chess_server/MessageHandler.java
+++ chess_server/src/main/java/com/chess_server/MessageHandler.java
@@ -196,12 +196,13 @@ public class MessageHandler {
         RegistrationStatus status = server.loginManager.registerUser(username, password);
         JsonObject response = new JsonObject();
         boolean success = status == RegistrationStatus.SUCCESS;
-        response.addProperty("type", "registerRes");
-        response.addProperty("status", status.name());
-        response.addProperty("username", success ? username : "");
         if (success) {
             connectionHandler.addActiveUserLoggedIn(clientId, conn, username); // login after registration
         }
+        response.addProperty("type", "registerRes");
+        response.addProperty("status", status.name());
+        response.addProperty("username", success ? username : "");
+
         server.sendMessageToClient(conn, response.toString());
     }
 
